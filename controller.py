9e480f539bfb0de4b27ad54a2e6c10b1c2593c98
Robert Swallow <rswallow23@gmail.com>
2016-01-29 22:39:18 -0500

    updated

--- controller.py
+++ controller.py
@@ -1,20 +1,37 @@
+
 from view import View 
 from model import * 
+import time
+import random
 
 class Game:
+	
+
 	def __init__(self):
 		self.view = View()
 		self.choice = ''
+		self.deck = Deck()
 		self.funds = None 
+		self.table = None
+		self.username = ''
 		self.user = UserDatabase()
+		self.quit = ["quit","q","QUIT"]
+		self.over = False
+
+	
+		
 
 	def run(self):
-		while self.choice not in ["quit","q","QUIT"]:
+		while self.choice not in self.quit:
 			self.start()
 			self.load_funds()
-		
+			self.menu()
+			self.play()
+							
 	def start(self):
 		login = self.view.intro()
+		self.username = login["username"]
+
 		while self.user.check_account_status(login):
 			break
 		else:
@@ -23,9 +40,96 @@ class Game:
 		
 	def load_funds(self):
 		self.funds = self.user.load_funds()
+
+	def menu(self):
+		self.choice = self.view.menu(name = self.username,funds = self.funds)
+		if self.choice == "1":
+			while self.ask_amount_to_bet():
+				break 
+		elif self.choice == "2":
+			self.transfer()
+		elif self.choice == "3":
+			self.choice = "quit"
+		else:
+			self.menu()
 		
+	def ask_amount_to_bet(self):
+		self.choice = self.view.ask_amount_to_bet()
+		while self.choice not in self.quit:
+			break 
+		if self.choice.isdigit():
+			self.choice = int(self.choice)
+			if self.funds >= self.choice and self.choice >1:
+				if self.choice % 100 == 0 or self.choice % 50 == 0 or self.choice % 20 == 0 or self.choice % 10 == 0 or self.choice % 5 == 0:
+					self.funds -= self.choice
+					self.table = self.choice
+					return True 
+				elif self.funds < self.choice:
+						self.view.insufficient_funds(funds = self.funds)
+						time.sleep(.5)
+						self.menu()
+				else:
+					self.view.invalid_bet_amount()
+					self.menu()
+		else:
+			self.view.incorrect_input()
+			self.menu()
+
+	def play(self):
+		c = self.deck.hit()
+		self.view.dealer_hit(c["face"])
+		self.choice = "quit"
+
+			
+
+		
+class Deck:
+	def __init__(self):
+		self.cards = []
+		suit = [" H", " S", " D"," C"]
+		card = ["2","3","3","4","5","6","7","8","9","10","J","Q","K","A"]
+
+		for c in card:
+			if not c.isdigit():
+				value = 10
+			else:
+				value = int(c)
+			ace = False if c != "A" else True 
+			for s in suit:
+				a_card = {"face":c+s,"value": value,"ace":ace}
+				self.cards.append(a_card)
+		random.shuffle(self.cards)
+
+	def hit(self):
+		return self.cards.pop()
+
+
+		
+		
+
+			
+
+
+
+
+		
+
 
 
 
 game=Game()
 game.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- view.py
+++ view.py
@@ -1,7 +1,9 @@
+import time
+
 class View:
 
 	def intro(self):
-		print("Welcome to BlackJack!")
+		print("Welcome to BlackJack!\n")
 		username = input("Enter username:")
 		password = input("Enter password:")
 		login = {"username":username,"password":password}
@@ -9,7 +11,53 @@ class View:
 
 	def no_account(self):
 		print("No Account Exists!")
+
+	def menu(self,name,funds):
+		choice = input("""
+\n{}, your available funds are ${}.
+
+	[1] Play BlackJack
+	[2] Transfer funds
+	[3] Quit
+
+
+""".format(name,funds))
+		return choice 
+
+	def ask_amount_to_bet(self):
+		print("Bills accepted are $100, $50, $20, $10, $5")
+		choice = input("Enter bet amount:$")
+
+		return choice
+
+	def incorrect_input(self):
+		print("\nIncorrect input! Enter an integer!!")
+
+	def insufficient_funds(self,funds):
+		print("\nInsufficient funds!")
+		print("Your balance is ${}".format(funds))
 		
+	def invalid_bet_amount(self):
+		print(
+		"""
+	___ 	___
+
+	    ____
+
+	    Bet a valid number please!
+		""")	
+
+	def dealer_hit(self, face):
+		print("House card is")
+		for x in [".",".",".\n"]:
+			time.sleep(.45)
+			print(x)
+		print(face)
+		
+
+	
+		
+
 
 
 
